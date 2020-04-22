a3cde9343083478844c0400c9c9a8616fe983272
ekyeremeh7 <ekyeremeh7@gmail.com>
2020-04-21 22:52:20 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ),
+  ));
+}
